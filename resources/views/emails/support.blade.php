a778a561aa690623f657099b838bbb6dd28e16a0
alenoosh <hope2end@gmail.com>
2015-10-03 11:19:48 +0330

    Text of email support is corrected.

--- resources/views/emails/support.blade.php
+++ resources/views/emails/support.blade.php
@@ -1,8 +1,7 @@
 {{$name}}.<br><br>
 
-I'm Ali from LaravelPanel , I'm going to help you with proccess of making your site with LaravelPanel.<br><br>
+I'm Ali from LaravelPanel, I'm going to help you with proccess of making your site with LaravelPanel.<br><br>
 
-When you have any questions or prolems just replay this <a href="mailto:support@serverfire.net">e-mail</a> and i will help you .<br><br> 
+Whenever you have questions or problems just reply this <a href="mailto:support@serverfire.net">e-mail</a> and i will help you.<br><br> 
 
-Thank you .<br>
-
+Thank you<br>
